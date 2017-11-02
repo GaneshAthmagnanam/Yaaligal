7ff46a3422935f8edb96ec35f984ef3edfa7985f
Athmagnanam <ganesh.athmagnanam@accenture.com>
2017-11-02 16:49:32 +0530

    overall contributors removed duplicate

--- src/pages/overall-contributors/overall-contributors.ts
+++ src/pages/overall-contributors/overall-contributors.ts
@@ -16,44 +16,50 @@ import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/databa
 export class OverallContributorsPage {
   contributorsDetails = [];
   filteredArray = [];
+  newArray = [];
+  flag: number = 0;
   constructor(public db: AngularFireDatabase, public navCtrl: NavController, public navParams: NavParams) {
     this.db.list('/userTransaction').subscribe(data => {
       // this.contributorsDetails.push([data[0].userName, data[0].userImage,data[0].userEmail])
       for (var i = 0; i < data.length; i++) {
-        
-        /*if(this.contributorsDetails.length==0){*/
-        this.contributorsDetails.push([data[i].userName, data[i].userImage,data[i].userEmail])
-     // }
-      /*else{
-        for(var j=0; j<this.contributorsDetails.length;j++){
-        if(data[i].userEmail != this.contributorsDetails[j][2]){
-          
-        this.contributorsDetails.push([data[i].userName, data[i].userImage,data[i].userEmail])
+        this.contributorsDetails.push([data[i].userName, data[i].userImage, data[i].userEmail])
       }
-      
+      for(var x=0;x<this.contributorsDetails.length;x++){
+        for(var y=x+1;y<this.contributorsDetails.length;y++){
+          if(this.contributorsDetails[x][2]==this.contributorsDetails[y][2]){
+            //this.contributorsDetails[x][0]=":";
+            //this.contributorsDetails[x][1]=":";
+            //this.contributorsDetails[x][2]=":";
+            this.contributorsDetails.splice(x,2);
+          }
         }
-      }*/
-      
       }
-      
-      /*for (var i = 0; i < data.length; i++) {
-        for(var j=0; j<this.contributorsDetails.length;j++){
-        if(data[i].userEmail!=this.contributorsDetails[j][2]){
-          
-        this.filteredArray.push([data[j].userName, data[j].userImage,data[j].userEmail])
-      }
-      
-        }
-      }*/
-      
-    })
-    console.log("@@@"+this.contributorsDetails.length);
-    //console.log("filtered"+this.filteredArray.length);
-    //this.contributorsDetails.filter((item, index,) => index > 2 )
-    //const uniqueNames = Array.from(new Set(this.contributorsDetails));
-    //console.log(uniqueNames);
-  }
 
+
+      /*this.filteredArray = this.contributorsDetails;
+      for (var l = 0; l < this.contributorsDetails.length; l++) {
+        for (var m = 0; m < this.filteredArray.length; m++) {
+          if (this.contributorsDetails[l][2] == this.filteredArray[m][2]) {
+            if (this.flag == 0) {
+              console.log("timesx" + m+"flag value is"+this.flag);
+              ++this.flag;
+              this.newArray.push([this.filteredArray[m][0], this.filteredArray[m][1], this.filteredArray[m][2]])
+            }
+          }
+        }
+        
+      }*/
+
+
+
+
+
+
+
+    })
+    console.log(this.contributorsDetails.length);
+
+  }
   ionViewDidLoad() {
     console.log('ionViewDidLoad OverallContributorsPage');
   }
